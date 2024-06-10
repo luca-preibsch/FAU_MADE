8175eb078d8e6a1d8e65c7fb9606802ab92e7318
Luca Preibsch <luca.preibsch@fau.de>
2024-06-10 15:46:49 +0200

    add basic component tests

--- project/tests/test_pipeline.py
+++ project/tests/test_pipeline.py
@@ -1,5 +1,7 @@
 import os
+import pandas as pd
 import pytest
+from pandas.testing import assert_frame_equal
 
 import project.data_pipeline as pipeline
 
@@ -8,22 +10,33 @@ expected_db_structure = {
     "energy_consumption": ['geo', 'TIME_PERIOD', 'OBS_VALUE'],
     "energy_share": ['geo', 'TIME_PERIOD', 'OBS_VALUE']
 }
+tests_dir = "./project/tests"
 
 
 @pytest.fixture
-def test_path():
-    return "./test-data.sqlite"
+def test_db_path():
+    return f"{tests_dir}/test-data.sqlite"
+
+
+@pytest.fixture
+def test_csv_path():
+    return f"{tests_dir}/test_csv.csv"
+
+
+@pytest.fixture
+def test_df(test_csv_path):
+    return pd.read_csv(test_csv_path, sep=",")
 
 
 # system level pipeline test
-def test_pipeline(test_path):
-    silent_remove(test_path)
-    assert os.path.isfile(test_path) is False  # test requires the database to not exist
+def test_pipeline(test_db_path):
+    silent_remove(test_db_path)
+    assert os.path.isfile(test_db_path) is False  # test requires the database to not exist
 
-    pipeline.main(test_path)
+    pipeline.main(test_db_path)
 
-    _test_pipeline_file_exists(test_path)
-    _test_pipeline_tables_exist(test_path)
+    _test_pipeline_file_exists(test_db_path)
+    _test_pipeline_tables_exist(test_db_path)
 
 
 def _test_pipeline_file_exists(file_path):
@@ -47,6 +60,20 @@ def _test_pipeline_tables_exist(file_path):
             assert expected_column in columns, f"Column {expected_column} does not exist in table {expected_table}."
 
     conn.close()
+
+
+def test_to_csv(test_csv_path, test_df):
+    data = pipeline.to_csv(test_csv_path)
+    assert_frame_equal(data, test_df)
+
+
+def test_load(tmp_path, test_df):
+    db_name = 'test_db'
+    sql_engine = f'sqlite:///{tmp_path}/test.sqlite'
+    pipeline.load(test_df, db_name, sql_engine)
+
+    loaded_data = pd.read_sql_table(db_name, sql_engine)
+    assert_frame_equal(loaded_data, test_df)
 
 
 def silent_remove(file_path):
